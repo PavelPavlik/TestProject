b4051537c5c53031238a4f48e8d0f554efce81e1
Pavel Pavlik <doublepavlik@gmail.com>
2023-02-13 14:06:01 +0300

    1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 	public static void main(String[] args) {
-
+		List<Employee>employeeList = new ArrayList<>();
+		try {
+			FileInputStream fis = new FileInputStream("D:\\text.txt");
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
 	}
 }
